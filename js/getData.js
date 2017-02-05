302118bbc9576b3eeac5e2bb359c3c4471b5c8f9
Rafael Cabral de Moura <rafael.xuxubelez@gmail.com>
2017-02-05 00:24:30 -0300

    Just Starting

--- js/getData.js
+++ js/getData.js
@@ -1,17 +1,9 @@
-function AJAX_JSON_Req(url, callback) {
-    var AJAX_req = new XMLHttpRequest();
-    AJAX_req.open("GET", url, true);
-    AJAX_req.setRequestHeader("Content-type", "application/json");
-
-    AJAX_req.onreadystatechange = function() {
-        if(AJAX_req.readyState == 4 && AJAX_req.status == 200){
-            callback(JSON.parse(AJAX_req.responseText));
-        }
-    };
-
-    AJAX_req.send();
+try {
+    JSONRequest.get("http://api.pgi.gov.br/api/1/serie/2426.json", function(sn, response, exception){
+            console.log(response);
+            console.log(exception);
+    });
 }
-
-AJAX_JSON_Req('http://api.pgi.gov.br/api/1/serie/2426.json', function (q) {
-  console.log(q);
-});
+catch(e) {
+    alert(e);
+}
